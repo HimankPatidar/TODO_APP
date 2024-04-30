c4e4d4753be9da9d37b1c66412de1cd70f0ae987
Himank Patidar <patidarhimank005@gmail.com>
2024-04-30 15:45:01 +0530

    updated

--- frontend/TODO/src/components/Todo.jsx
+++ frontend/TODO/src/components/Todo.jsx
@@ -1,41 +1,4 @@
 
-   
-// // export function Todos(todos) {
-// //     return (
-// //         <div>
-// //             {todos.map((todo) => (
-// //                 <div key={todo.id}>
-// //                     <h1>{todo.title}</h1>
-// //                     <p>{todo.description}</p>
-// //                     <button>{todo.completed === true ? "Completed" : "Mark as Complete"}</button>
-// //                 </div>
-// //             ))}
-// //         </div>
-// //     );
-// // }
-
-
-
-// import PropTypes from 'prop-types';
-
-// export function Todos({ todos }) {
-//     return (
-//         <div>
-//             {todos.map((todo) => (
-//                 <div key={todo.id}>
-//                     <h1>{todo.title}</h1>
-//                     <p>{todo.description}</p>
-//                     <button>{todo.completed === true ? "Completed" : "Mark as Complete"}</button>
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// }
-
-// Todos.propTypes = {
-//     todos: PropTypes.array.isRequired,
-// };
-
 
 export function Todos({ todos }) {
     // Check if todos is undefined or null before mapping
